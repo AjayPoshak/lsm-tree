188a2dde12aff27057c144ff263dd38c4f855527
Ajay Poshak <poshakajay@gmail.com>
2025-03-02 18:47:41 +0530

    Refactor ReadLineByLine in files package

--- files/files.go
+++ files/files.go
@@ -34,52 +34,38 @@ func NewFile(path string) *File {
 	}
 }
 
-func (file *File) ReadFileByLine(key string) (string, string) {
+type NextLine func() string
+
+func (file *File) ReadFileByLine(key string) (NextLine, error) {
 	filePtr, err := os.Open(file.path)
 	if err != nil {
 		log.Fatal(err)
-		return "", ""
+		return nil, err
 	}
-	defer filePtr.Close()
-	// Instead of reading entire file, read it line by line so we don't need to read entire file into memory at once
 	scanner := bufio.NewScanner(filePtr)
-  value := ""
-	for scanner.Scan() {
-		line := scanner.Text()
-    // A reasonable assumption here is about delimiter. Usually delimiters are something which won't occur in key or value so that key and value can be separated in any given string.
-    // But there are a lot of edge cases for example what if key name or value contains the delimiter, it will break the parsing.  To fix this, we are wrapping
-    // key and value in double quotes, and ignore everything inside the quotes while parsing.
-		pair := strings.Split(line, "\"=") // Looking for "= to separate out key and value
-    // After parsing the given string which looks like this ["key, "value"], we need to remove double quote at the beginning for the key, & remove quotes from beginning and end for the value.
-    strippedKey := pair[0][1:]
-    strippedValue := pair[1][1:len(pair[1])-1]
-		if strings.TrimSpace(strippedKey) == key {
-      value = strippedValue
+	return func() string {
+		if scanner.Scan() {
+			return scanner.Text()
 		}
-	}
-
-	er := scanner.Err()
-	if er != nil {
-		log.Fatalf("Error in reading lines %v", er)
-	return "", ""
-	}
-  return key, value
+		filePtr.Close()
+		return ""
+	}, nil
 }
 
 func (file *File) CountKeys() int {
-  filePtr, err := os.Open(file.path)
-  if err != nil {
-    log.Fatal(err)
-    return 0
-  }
-  defer filePtr.Close()
-  scanner := bufio.NewScanner(filePtr)
-  count := 0
-  for scanner.Scan() {
-    scanner.Text()
-    count++
-  }
-  return count
+	filePtr, err := os.Open(file.path)
+	if err != nil {
+		log.Fatal(err)
+		return 0
+	}
+	defer filePtr.Close()
+	scanner := bufio.NewScanner(filePtr)
+	count := 0
+	for scanner.Scan() {
+		scanner.Text()
+		count++
+	}
+	return count
 }
 
 func (file *File) AppendToFile(key, value string) {
@@ -89,7 +75,7 @@ func (file *File) AppendToFile(key, value string) {
 		return
 	}
 	defer filePtr.Close()
-		textToBeWritten := fmt.Sprintf("\"%s\"=\"%s\"\n", key, value)
+	textToBeWritten := fmt.Sprintf("\"%s\"=\"%s\"\n", key, value)
 	_, err = filePtr.WriteString(textToBeWritten)
 	if err != nil {
 		log.Fatalf("Error writing to file %v", err)
